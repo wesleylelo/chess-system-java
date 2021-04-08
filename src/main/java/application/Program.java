0af1888d9ee9575d3f820a16ea26d23b933071f4
Wesley Santos <wesleylelo0078@gmail.com>
2021-04-07 22:55:08 -0300

    Project created

---
+++ src/main/java/application/Program.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package application;
+
+/**
+ *
+ * @author lelo0
+ */
+public class Program {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello Word");
+    }
+    
+}
