70c1c43441a0aa3244c0cdb959bc864d9a16a1ef
Wesley Santos <wesleylelo0078@gmail.com>
2021-04-07 23:03:16 -0300

    First Class: Position

--- src/main/java/application/Program.java
+++ src/main/java/application/Program.java
@@ -5,6 +5,8 @@
  */
 package application;
 
+import boardgame.Position;
+
 /**
  *
  * @author lelo0
@@ -15,7 +17,8 @@ public class Program {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("Hello Word");
+        Position pos = new Position(3, 5);
+        System.out.println(pos);
     }
     
 }
---
+++ src/main/java/boardgame/Position.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package boardgame;
+
+/**
+ *
+ * @author lelo0
+ */
+public class Position {
+    private int row;
+    private int column;
+
+    public Position(int row, int column) {
+        this.row = row;
+        this.column = column;
+    }
+
+    public int getRow() {
+        return row;
+    }
+
+    public void setRow(int row) {
+        this.row = row;
+    }
+
+    public int getColumn() {
+        return column;
+    }
+
+    public void setColumn(int column) {
+        this.column = column;
+    }
+    
+    @Override
+    public String toString(){
+        return row + ", " + column;
+    }
+}
