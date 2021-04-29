b8fd05de677c6ab1573562e5a0b1a18cd9433a4b
Wesley Santos <wesleylelo0078@gmail.com>
2021-04-29 18:03:53 -0300

    Handling exceptions and clearing screen

--- src/main/java/boardgame/Board.java
+++ src/main/java/boardgame/Board.java
@@ -51,6 +51,18 @@ public class Board {
         pieces[position.getRow()][position.getColumn()] = piece;
         piece.position = position;
     }
+    public Piece removePiece (Position position){
+        if(!positionExists(position)){
+            throw new BoardException("Position not on the board");
+        }
+        if(piece(position) == null){
+            return null;
+        }
+        Piece aux = piece(position);
+        aux.position = null;
+        pieces[position.getRow()][position.getColumn()] = null;
+        return aux;
+    }
     public boolean positionExists(Position position){
        return positionExists(position.getRow(), position.getColumn()); 
     }
