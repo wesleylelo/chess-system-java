ac8be53fd6fa53b0dcdfdb9a97b46ad3375958c4
Wesley Santos <wesleylelo0078@gmail.com>
2021-05-03 19:13:42 -0300

    Rook

--- src/main/java/chess/ChessPiece.java
+++ src/main/java/chess/ChessPiece.java
@@ -14,7 +14,7 @@ public abstract class ChessPiece extends Piece {
     public Color getColor() {
         return color;
     }
-    protected boolean isTherOpponentPiece(Position position){
+    protected boolean isThereOpponentPiece(Position position){
         ChessPiece p = (ChessPiece)getBoard().piece(position);
         return p != null && p.getColor() != color;
     }
