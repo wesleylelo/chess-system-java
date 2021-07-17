c9ee7f45b75acfa55b4a8ef3a89b45bfa10afeb6
Wesley <wesleylelo0078@gmail.com>
2021-07-17 09:18:11 -0300

    Atualização --

--- src/main/java/chess/ChessMatch.java
+++ src/main/java/chess/ChessMatch.java
@@ -11,6 +11,8 @@ import boardgame.Position;
 import static chess.Color.BLACK;
 import static chess.Color.WHITE;
 import chess.pieces.Rook;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  *
@@ -20,6 +22,9 @@ public class ChessMatch {
     private int turn;
     private Color currentPlayer;
     private Board board;
+    
+    private List<Piece> piecesOnTheBoard = new ArrayList<>();
+    private List<Piece> capturedPieces = new ArrayList<>();
     public ChessMatch(){
         board = new Board(8,8);
         turn = 1;
@@ -60,6 +65,10 @@ public class ChessMatch {
         Piece p = board.removePiece(source);
         Piece capturedPiece = board.removePiece(target);
         board.placePiece(p, target);
+        if(capturedPiece != null){
+            piecesOnTheBoard.remove(capturedPiece);
+            capturedPieces.add(capturedPiece);
+        }
         return capturedPiece;
     }
     private void validateSourcePosition(Position position){
@@ -89,6 +98,7 @@ public class ChessMatch {
     }
     private void placeNewPiece(char column, int row, ChessPiece piece){
         board.placePiece(piece, new ChessPosition(column, row).toPosition());
+        piecesOnTheBoard.add(piece);
     }
     private void initialSetup(){
         placeNewPiece('a', 1, new Rook(board, Color.WHITE) {});
