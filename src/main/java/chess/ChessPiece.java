67eec68d30dec72860a21ad8af2a8e87229a62f9
Wesley <wesleylelo0078@gmail.com>
2021-07-17 11:04:27 -0300

    Atualização --

--- src/main/java/chess/ChessPiece.java
+++ src/main/java/chess/ChessPiece.java
@@ -5,7 +5,7 @@ import boardgame.Piece;
 import boardgame.Position;
 public abstract class ChessPiece extends Piece {
     private Color color;
-
+    private int moveCount;
     public ChessPiece(Color color, Board board) {
         super(board);
         this.color = color;
@@ -13,6 +13,12 @@ public abstract class ChessPiece extends Piece {
 
     public Color getColor() {
         return color;
+    }
+    public void increaseMoveCount(){
+        moveCount++;
+    }
+    public void decreaseMoveCount(){
+        moveCount--;
     }
     public ChessPosition getChessPosition(){
         return ChessPosition.fromPosition(position);
